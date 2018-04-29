1e00d3eeb7309ba773f05dfcf738e21c87022027
you <you@yous-MacBook.home>
2018-04-29 01:17:36 -0400

    final push

--- liri.js
+++ liri.js
@@ -1,4 +1,3 @@
-// Screen shot the app working and add into repo
 
 // Install npm Twitter
 // Done
@@ -32,12 +31,11 @@ var input = process.argv;
 var a = input[2];
 var b = input[3];
 
+// Test Input: node liri my-tweets
 // This will show your last 20 tweets and when they were created at in your terminal/bash window.
 if (a === "my-tweets") {
     var params = { screen_name: 'ChrisHantis', limit: 20 };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
-
-        // Test Input: node liri my-tweets
 
         //console.log(tweets);
         console.log(JSON.stringify(tweets, null, 2));
@@ -45,35 +43,47 @@ if (a === "my-tweets") {
 
 }
 
+//Test Input: node liri spotify-this-song 
+
 else if (a === "spotify-this-song") {
-    spotify.search({ type: "track", query: b, limit: 5 })
-        .then(function (response) {
-            //console.log(JSON.stringify(response, null, 2));
+    if (b) {
+        spotify.search({ type: "track", query: b, limit: 5 })
+            .then(function (response) {
+                //console.log(JSON.stringify(response, null, 2));
 
-            //Artist(s)
-            console.log('Artist Name: ', JSON.stringify(response.tracks.items[0].artists[0].name, null, 2));
+                //Artist(s)
+                console.log('Artist Name: ', JSON.stringify(response.tracks.items[0].artists[0].name, null, 2));
 
-            // //Song Name
-            console.log('Song Name: ', JSON.stringify(response.tracks.items[0].name, null, 2));
+                // //Song Name
+                console.log('Song Name: ', JSON.stringify(response.tracks.items[0].name, null, 2));
 
-            // //Song Preview Link
-            console.log('Preview Song Link: ', JSON.stringify(response.tracks.items[0].external_urls.spotify, null, 2));
+                // //Song Preview Link
+                console.log('Preview Song Link: ', JSON.stringify(response.tracks.items[0].external_urls.spotify, null, 2));
 
-            // //Album, Song Is From
-            console.log('Album: ', JSON.stringify(response.tracks.items[0].album.name, null, 2));
+                // //Album, Song Is From
+                console.log('Album: ', JSON.stringify(response.tracks.items[0].album.name, null, 2));
 
-        })
+            })
+    }
+    else {
+        spotify.search({ type: "track", query: "Ace of Base The Sign", limit: 1 })
+            .then(function (response) {
+                console.log(JSON.stringify(response, null, 2));
 
-        .catch(function (err) {
-            spotify.search({ type: "track", query: "The Sign" })
-            console.log(JSON.stringify(err, null, 2));
-            console.log(err);
-        })
+                //Artist(s)
+                console.log('Artist Name: ', JSON.stringify(response.tracks.items[0].artists[0].name, null, 2));
 
-        .catch(function (fail) {
-            spotify.search({ type: "track", query: "The Sign" })
-            console.log(JSON.stringify(fail, null, 2));
-        });
+                // //Song Name
+                console.log('Song Name: ', JSON.stringify(response.tracks.items[0].name, null, 2));
+
+                // //Song Preview Link
+                console.log('Preview Song Link: ', JSON.stringify(response.tracks.items[0].external_urls.spotify, null, 2));
+
+                // //Album, Song Is From
+                console.log('Album: ', JSON.stringify(response.tracks.items[0].album.name, null, 2));
+                
+            })
+    }
 }
 
 else if (a === "movie-this") {
@@ -111,7 +121,7 @@ else if (a === "movie-this") {
         })
     }
 
-        // Test Input: node liri movie-this
+    // Test Input: node liri movie-this
 
     else {
         console.log("If you haven't watched Mr. Nobody... then you should watch it on Netflix");
@@ -125,21 +135,21 @@ else if (a === "do-what-it-says") {
     //     if (err) throw err;
     //     console.log('This is the data in the .txt file: ', data);
 
-        spotify.search({ type: "track", query: "I Want it That Way", limit: 5 })
-            .then(function (response) {
+    spotify.search({ type: "track", query: "I Want it That Way", limit: 5 })
+        .then(function (response) {
 
-                //Artist(s)
-                console.log('Artist Name: ', JSON.stringify(response.tracks.items[0].artists[0].name, null, 2));
+            //Artist(s)
+            console.log('Artist Name: ', JSON.stringify(response.tracks.items[0].artists[0].name, null, 2));
 
-                // //Song Name
-                console.log('Song Name: ', JSON.stringify(response.tracks.items[0].name, null, 2));
+            // //Song Name
+            console.log('Song Name: ', JSON.stringify(response.tracks.items[0].name, null, 2));
 
-                // //Song Preview Link
-                console.log('Preview Song Link: ', JSON.stringify(response.tracks.items[0].external_urls.spotify, null, 2));
+            // //Song Preview Link
+            console.log('Preview Song Link: ', JSON.stringify(response.tracks.items[0].external_urls.spotify, null, 2));
 
-                // //Album, Song Is From
-                console.log('Album: ', JSON.stringify(response.tracks.items[0].album.name, null, 2));
+            // //Album, Song Is From
+            console.log('Album: ', JSON.stringify(response.tracks.items[0].album.name, null, 2));
 
-            });
-        //})
-    }
+        });
+    //})
+}
