0ac7c51a56b68c45b91b5327a5c2acc5fe6e0e1d
you <you@yous-MacBook.home>
2018-04-29 00:31:45 -0400

    another great update

--- liri.js
+++ liri.js
@@ -20,6 +20,7 @@ var keys = require("./key.js");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require("request");
+var fs = require("fs");
 
 var client = new Twitter(keys.twitter);
 var spotify = new Spotify(keys.spotify);
@@ -33,13 +34,13 @@ var b = input[3];
 
 // This will show your last 20 tweets and when they were created at in your terminal/bash window.
 if (a === "my-tweets") {
-    var params = { screen_name: 'ChrisHantis' };
+    var params = { screen_name: 'ChrisHantis', limit: 20 };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
-        if (!error) {
-            //console.log(tweets);
-            //console.log(JSON.stringify(response, null, 2));
-            console.log(JSON.stringify(tweets, null, 2));
-        }
+
+        // Test Input: node liri my-tweets
+
+        //console.log(tweets);
+        console.log(JSON.stringify(tweets, null, 2));
     });
 
 }
@@ -50,16 +51,16 @@ else if (a === "spotify-this-song") {
             //console.log(JSON.stringify(response, null, 2));
 
             //Artist(s)
-            console.log(JSON.stringify(response.tracks.items[0].artists[0].name, null, 2));
+            console.log('Artist Name: ', JSON.stringify(response.tracks.items[0].artists[0].name, null, 2));
 
             // //Song Name
-            console.log(JSON.stringify(response.tracks.items[0].name, null, 2));
+            console.log('Song Name: ', JSON.stringify(response.tracks.items[0].name, null, 2));
 
             // //Song Preview Link
-            console.log(JSON.stringify(response.tracks.items[0].external_urls.spotify, null, 2));
+            console.log('Preview Song Link: ', JSON.stringify(response.tracks.items[0].external_urls.spotify, null, 2));
 
             // //Album, Song Is From
-            console.log(JSON.stringify(response.tracks.items[0].album.name, null, 2));
+            console.log('Album: ', JSON.stringify(response.tracks.items[0].album.name, null, 2));
 
         })
 
@@ -76,44 +77,48 @@ else if (a === "spotify-this-song") {
 }
 
 else if (a === "movie-this") {
-    console.log(b);
-    request("https://www.omdbapi.com/?t=" + b + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+    if (b) {
+        request("https://www.omdbapi.com/?t=" + b + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
 
-        // If the request was successful...
-        if (!error && response.statusCode === 200) {
+            // Test Input: node liri movie-this babe
+
+            // If the request was successful...
+            // if (!error && response.statusCode === 200) {
 
             // Title of the movie.
-            console.log(JSON.parse(body).Title);
+            console.log('Movie Title: ', JSON.parse(body).Title);
 
             // Year the movie came out.
-            console.log(JSON.parse(body).Title);
+            console.log('Year: ', JSON.parse(body).Year);
 
             // IMDB Rating of the movie.
-            console.log(JSON.parse(body).Title);
+            console.log('IMDB Rating: ', JSON.parse(body).Ratings[0]);
 
             // Rotten Tomatoes Rating of the movie.
-            console.log(JSON.parse(body).Title);
+            console.log('Rotten Tomatoes: ', JSON.parse(body).Ratings[1]);
 
             // Country where the movie was produced.
-            console.log(JSON.parse(body).Title);
+            console.log('Country of Origin: ', JSON.parse(body).Country);
 
             // Language of the movie.
-            console.log(JSON.parse(body).Title);
+            console.log('Language: ', JSON.parse(body).Language);
 
             // Plot of the movie.
-            console.log(JSON.parse(body).Title);
+            console.log('Plot: ', JSON.parse(body).Plot);
 
             // Actors in the movie.
-            console.log(JSON.parse(body).Title);
+            console.log('Actors: ', JSON.parse(body).Actors);
+        })
+    }
 
-        }
-        else {
-            console.log("If you haven't watch Mr. Nobody then you should watch it on Netflix <a href='http://www.imdb.com/title/tt0485947/' target='_blank'");
-        }
-        // node liri movie-this babe
-    });
+    // Test Input: node liri movie-this
+
+    else {
+        console.log("If you haven't watched Mr. Nobody... then you should watch it on Netflix");
+    }
+}
 
 
-// else if (a === "do-what-it-says") {
-//     console.log('do-what-it-says');
-// }
+else if (a === "do-what-it-says") {
+    console.log('do-what-it-says');
+}
